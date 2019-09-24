0fc61756b00de16fef397e1f5571c79052dfa75a
Your Name <mwaurarita2019@gmail.com>
2019-09-24 18:02:54 +0300

    add final changes

--- app/request.py
+++ app/request.py
@@ -3,6 +3,7 @@ from config import Config
 from .models import Quotes
 quotes_url = Config.QUOTES_URL
 def getQuotes():
+    
    random_quote = requests.get(quotes_url)
    new_quote = random_quote.json()
    author = new_quote.get("author")
