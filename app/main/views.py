a93fcc2cfe4b92a1a130774fa1bf738bc6afdcbb
Your Name <mwaurarita2019@gmail.com>
2019-09-24 16:06:24 +0300

    first commit

--- app/main/views.py
+++ app/main/views.py
@@ -33,39 +33,6 @@ def index():
     
     random=urllib.request.urlopen('http://quotes.stormconsultancy.co.uk/random.json').read()
     get_source_response = json.loads(random)
-blog
-This is a personal blogging website where you can create and share your opinions and other users can read and comment on them. This is an app that allows users to create and post their opinions on any topic of their interest. Other users can then read and comment on the opinions.
-
-By Rita mwaura
-BDD SPECIFICATIONS TABLE
-User	User Action	Output
-Writer	Create an Account	Fill the registration form and sign up
-Write new blog posts	Click on Add a new Blog to add a new post
-Delete a blog post	On the blog post, click on the delete post button
-Delete comments from a post	On the comments, click the delete comment button
-User	View Blog post on the site	Go to the home page to view the blog posts
-Comment on a blog post	Click comment on any post
-Subscribe to the blog	Fill in the subscription form and submit
-Setup/Installation requirements
-Ensure you have Python3.6 installed on your computer.
-Clone this respository and navigate to where it has been saved.
-Open the file in your editor and navigate to the start.sh file.
-Enter your email address, email password and secret key.
-Activate the virtual environment using this command: source virtual/bin/activate
-Run chmod a+x on the terminal
-Run ./start.sh on the terminal to open the app
-Known Bugs
-There are currently no known bugs in the application
-
-Technologies Used
-Python3.6
-Flask
-HTML
-Bootstrap
-Support and Contact Details
-For more information, questions or help, feel free to reach me on email me at : emmanuel.muchiri@outlook.com
-
-
     return render_template('index.html',index=index, title=title, post=post, random=get_source_response)
 
 @main.route('/user/<uname>')
